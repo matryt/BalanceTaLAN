2dad86e1481a735d1cf615845c22ca381c97c6ba
Maksimenka Matvei <70953270+MatveiMKS@users.noreply.github.com>
2025-04-09 17:07:53 +0200

    Menu Fragment

--- app/app/src/main/java/edu/polytech/balancetalan/MenuFragment.java
+++ app/app/src/main/java/edu/polytech/balancetalan/MenuFragment.java
@@ -2,31 +2,94 @@ package edu.polytech.balancetalan;
 
 import android.content.Context;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.fragment.app.Fragment;
+import java.util.ArrayList;
+import java.util.List;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * create an instance of this fragment.
- */
-public class MenuFragment extends Fragment {
-    private final String TAG = "frallo " + getClass().getSimpleName();
+public class MenuFragment extends Fragment{
+    private final String TAG = "balance "+getClass().getSimpleName();
     private Menuable menuable;
     private int currentActivatedIndex = 0;
 
+
     public MenuFragment() {
+        Log.d(TAG, "MenuFragment created");
     }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,  Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_menu, container, false);
+        List<ImageView> imageViews = findPicturesMenuFromId( view.findViewById(R.id.itemsMenu));
+
+        //get current activated index menu
+        if (getArguments() != null) {
+            currentActivatedIndex = getArguments().getInt(getString(R.string.index), 1)-1;  //convert menu number to index
+        }
+        imageViews.get(currentActivatedIndex).setImageResource(  view.getResources().getIdentifier("menu"+(currentActivatedIndex+1)+"_s", "drawable", view.getContext().getPackageName()) );
+        Log.d(TAG, "BEGIN : menu index " +  currentActivatedIndex + " is selected");
+
+        //notify activity the menu is selected
+        menuable.onMenuChange(currentActivatedIndex);
+
+        TextView text = view.findViewById(R.id.txtFragmentMenu);
+        text.setText("Menu");
+        // menuable.onMenuChange(currentActivatedIndex);
+
+
+        for(ImageView imageView : imageViews) {
+            imageView.setOnClickListener( menu -> {
+                int oldIndex = currentActivatedIndex;
+                currentActivatedIndex = Integer.parseInt(imageView.getTag().toString())-1;
+
+                //notify activity currentIndexChange
+                menuable.onMenuChange(currentActivatedIndex);
+
+                //display old menu in gray
+                imageViews.get(oldIndex).setImageResource(  view.getResources().getIdentifier("menu"+(oldIndex+1), "drawable", view.getContext().getPackageName()) );
+
+                //display new menu in green
+                ((ImageView)menu).setImageResource(  view.getResources().getIdentifier("menu"+(currentActivatedIndex+1)+"_s", "drawable", view.getContext().getPackageName()) );
+            });
+        }
+        return view;
+    }
+
+
+
+
+
+    // browse rootView and sort all buttons in "buttons" list
+    private List<ImageView> findPicturesMenuFromId(View view) {
+        List<ImageView> pictures = new ArrayList<>();
+        if (view instanceof ViewGroup) {
+            ViewGroup viewGroup = (ViewGroup) view;
+            int count = viewGroup.getChildCount();
+            for (int i = 0; i < count; i++) {
+                View child = viewGroup.getChildAt(i);
+                if (child instanceof ImageView) {
+                    String idString = getResources().getResourceEntryName(child.getId());
+                    if (idString.matches("menu[1-9]?")) {
+                        pictures.add((ImageView) child);
+                    }
+                }
+            }
+        }
+        return pictures;
+    }
+
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+
         if (requireActivity() instanceof Menuable) {
             menuable = (Menuable) requireActivity();
             Log.d(TAG, "Class " + requireActivity().getClass().getSimpleName() + " implements Notifiable.");
@@ -35,16 +98,4 @@ public class MenuFragment extends Fragment {
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_menu, container, false);
-        //get current activated index menu
-        if (getArguments() != null) {
-            currentActivatedIndex = getArguments().getInt(getString(R.string.index), 1) - 1;  //convert menu number to index
-        }
-
-        //notify activity the menu is selected
-        menuable.onMenuChange(currentActivatedIndex);
-        return view;
-    }
 }
