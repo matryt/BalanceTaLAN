42d8c5137310c6cd208399e644167d9eeea0d29c
Maksimenka Matvei <70953270+MatveiMKS@users.noreply.github.com>
2025-05-19 22:20:28 +0200

    Merge 26b5703a143b57210dae7e8b8336794faa7c9223

--- app/app/src/main/java/edu/polytech/balancetalan/Screen1Fragment.java
+++ app/app/src/main/java/edu/polytech/balancetalan/Screen1Fragment.java
@@ -1,10 +1,21 @@
 package edu.polytech.balancetalan;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
+import android.provider.Settings;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -14,6 +25,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -44,12 +56,46 @@ public class Screen1Fragment extends Fragment implements AdapterView.OnItemSelec
     private Notifiable notifiable;
     private final String TAG = "BalanceTaLan " + getClass().getSimpleName();
 
+    private ActivityResultLauncher<Intent> takePictureLauncher;
+
+    private ActivityResultLauncher<String> requestPermissionLauncher;
+
    public Screen1Fragment() {
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "Fragment created");
         super.onCreate(savedInstanceState);
+
+        // Demander la permission caméra
+        requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+            if (isGranted) {
+                // Lancer la prise de photo si la permission est accordée
+                takePicture();
+            } else {
+                Log.d(TAG, "No permission granted");
+                explain();
+            }
+        });
+
+        // Initialisation de l'ActivityResultLauncher pour prendre une photo
+        takePictureLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if (result.getResultCode() == getActivity().RESULT_OK && result.getData() != null) {
+                // Récupérer l'image et l'afficher
+                Bitmap photo = (Bitmap) result.getData().getExtras().get("data");
+
+                // Trouver le ImageView et le bouton
+                ImageView imageViewPhoto = getView().findViewById(R.id.imageViewPhoto);
+                imageViewPhoto.setImageBitmap(photo); // Afficher la photo
+
+                // Rendre le bouton invisible
+                getView().findViewById(R.id.buttonImage).setVisibility(View.GONE);
+
+                // Rendre l'ImageView visible
+                imageViewPhoto.setVisibility(View.VISIBLE);
+            }
+        });
     }
 
     @Override
@@ -63,6 +109,18 @@ public class Screen1Fragment extends Fragment implements AdapterView.OnItemSelec
         Button validateButton = view.findViewById(R.id.send_ticket_button);
         setupTextValidationButton(view, validateButton);
         setupTicketSending(view, validateButton, spinner);
+
+        // Lorsque le bouton image est cliqué
+        view.findViewById(R.id.buttonImage).setOnClickListener(v -> {
+            // Vérifier si la permission caméra est accordée
+            if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "Permission granted");
+                takePicture(); // Prendre la photo si la permission est accordée
+            } else {
+                Log.d(TAG, "Launching permission request");
+                requestPermissionLauncher.launch(Manifest.permission.CAMERA); // Demander la permission si non accordée
+            }
+        });
 
         return view;
     }
@@ -174,4 +232,34 @@ public class Screen1Fragment extends Fragment implements AdapterView.OnItemSelec
     public void onNothingSelected(AdapterView<?> parent) {
         // TODO Auto-generated method stub
     }
+
+
+    private void explain() {
+        if (!shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+            new AlertDialog.Builder(getContext())
+                    .setTitle("Permission bloquée")
+                    .setMessage("La permission caméra a été \"définitivement\" refusée. Vous devez l'activer à nouveau \"manuellement\" depuis les paramètres.")
+                    .setPositiveButton("Ouvrir les paramètres", (dialog, which) -> {
+                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        Uri uri = Uri.fromParts("package", getActivity().getPackageName(), null);
+                        intent.setData(uri);
+                        startActivity(intent);
+                    })
+                    .setNegativeButton("Annuler", (dialog, which) -> dialog.dismiss())
+                    .show();
+        } else {
+            new AlertDialog.Builder(getContext())
+                    .setTitle("Permission CAMERA refusée")
+                    .setMessage("Impossible d'utiliser la caméra sans permission. Redemander l'autorisation ?")
+                    .setPositiveButton("Redemander", (dialog, which) -> requestPermissionLauncher.launch(Manifest.permission.CAMERA))
+                    .setNegativeButton("NON !", null)
+                    .show();
+        }
+    }
+
+    public void takePicture() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        takePictureLauncher.launch(intent);
+    }
+
 }
